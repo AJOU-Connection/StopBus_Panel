67669764541c2ac8417d8855c2954815e4451f07
mjin1220 <myeongjin1220@gmail.com>
2018-05-18 02:00:16 +0900

    add GetDataExample.java

---
+++ src/panel/model/GetDataExample.java
@@ -0,0 +1,47 @@
+package panel.model;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetDataExample {
+	public String getArrivalTimes(String stationNumber){
+		String result = "";
+		
+		
+		try {
+			String targetURL = "http://stop-bus.tk/user/busArrival";
+			URL url = new URL(targetURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST"); // 보내는 타입
+
+			// 데이터
+			String param = "{\"districtCd\":2, \"stationNumber\":\"" + stationNumber + "\"}";
+
+			// 전송
+			OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+			osw.write(param);
+			osw.flush();
+
+			// 응답
+			BufferedReader br = null;
+			br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+
+			// 닫기
+			osw.close();
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+}
